798c95d9ec3a42382eb79d4089025af893736d51
JonathanDudzik <jonathan.d.wesley@gmail.com>
2022-11-18 11:15:23 -0500

    breaking point: going with a template

--- go.mod
+++ go.mod
@@ -8,20 +8,31 @@ require (
 )
 
 require (
+	github.com/Azure/azure-sdk-for-go/sdk/azcore v1.2.0 // indirect
+	github.com/Azure/azure-sdk-for-go/sdk/azidentity v1.2.0 // indirect
+	github.com/Azure/azure-sdk-for-go/sdk/internal v1.0.0 // indirect
+	github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/network/armnetwork v1.1.0 // indirect
+	github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/network/armnetwork/v2 v2.0.1 // indirect
+	github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/resources/armresources v1.0.0 // indirect
+	github.com/AzureAD/microsoft-authentication-library-for-go v0.7.0 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-playground/validator/v10 v10.11.1 // indirect
 	github.com/goccy/go-json v0.9.11 // indirect
+	github.com/golang-jwt/jwt/v4 v4.4.2 // indirect
+	github.com/google/uuid v1.1.1 // indirect
 	github.com/gorilla/context v1.1.1 // indirect
 	github.com/gorilla/securecookie v1.1.1 // indirect
 	github.com/gorilla/sessions v1.2.1 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/kylelemons/godebug v1.1.0 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/mattn/go-isatty v0.0.16 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.5 // indirect
+	github.com/pkg/browser v0.0.0-20210115035449-ce105d075bb4 // indirect
 	github.com/ugorji/go/codec v1.2.7 // indirect
 	golang.org/x/crypto v0.1.0 // indirect
 	golang.org/x/net v0.1.0 // indirect
--- main.go
+++ main.go
@@ -1,62 +1,176 @@
 package main
 
 import (
-	"github.com/gin-contrib/sessions"
-	"github.com/gin-contrib/sessions/cookie"
-	"github.com/gin-gonic/gin"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
 
-	globals "github.com/jonathandudzik/gin_session_auth/globals"
-	middleware "github.com/jonathandudzik/gin_session_auth/middleware"
-	routes "github.com/jonathandudzik/gin_session_auth/routes"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/network/armnetwork"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/resources/armresources"
 )
 
+// show debugging
+// show exploring code through VSCode (hover and finding definitions) and browser docs
 func main() {
-	router := gin.Default()
+	cred, err := azidentity.NewAzureCLICredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain CLI credential: %v", err)
+	}
 
-	store := cookie.NewStore(globals.Secret)
-	router.Use(sessions.Sessions("cookie", store))
+	// First just use a string
+	// second use this library (first library)
+	subscriptionID := os.Getenv("AZURE_SUBSCRIPTION_ID")
+	resourceGroupName := os.Getenv("AZURE_RESOURCE_GROUP")
+	resourceGroupLocation := os.Getenv("AZURE_RESOURCE_LOCATION")
+	ctx := context.Background()
 
-	public := router.Group("/")
-	routes.PublicRoutes(public)
+	// at first do not return anything
+	// then return just an error (no new vars)
+	// then return an error and a value
+	err = createResourceGroup(ctx, cred, subscriptionID, resourceGroupName, resourceGroupLocation)
 
-	private := router.Group("/")
-	private.Use(middleware.AuthRequired)
-	routes.PrivateRoutes(private)
+	// simple err first, then formatted
+	if err != nil {
+		log.Fatalf("Failed at createResourceGroup: %v", err)
+	} //show with a then statement but then explain the convention of just a new line before the if err message
 
-	router.Run("localhost:8080")
+	err = createVirtualNetwork(ctx, cred, subscriptionID, resourceGroupName, resourceGroupLocation)
+	if err != nil {
+		log.Fatalf("Failed createVirtualNetwork: %v", err)
+	}
+
+	templateFile := "template.json"
+	deploymentName := "deployARM-how-to"
+
+	template, err := readJSON(templateFile)
+	if err != nil {
+		return
+	}
+
+	deploymentsClient, err := armresources.NewDeploymentsClient(subscriptionID, cred, nil)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+
+	poller, err := deploymentsClient.BeginCreateOrUpdate(
+		ctx,
+		resourceGroupName,
+		deploymentName,
+		armresources.Deployment{
+			Properties: &armresources.DeploymentProperties{
+				Mode:       to.Ptr(armresources.DeploymentModeIncremental),
+				Parameters: map[string]interface{}{},
+				Template:   template,
+			},
+		},
+		nil,
+	)
+	if err != nil {
+		log.Fatalf("failed to deploy template: %v", err)
+	}
+
+	res, err := poller.PollUntilDone(ctx, nil)
+	if err != nil {
+		log.Fatalf("failed to pull the result: %v", err)
+	}
+
+	fmt.Println(res)
 }
 
-// func checkResourceGroup(c *gin.Context) {
-// 	// router.GET("/check-resource-group/:resourceGroupName", checkResourceGroup)
-// 	resourceGroupName := c.Param("resourceGroupName")
+func readJSON(path string) (map[string]interface{}, error) {
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		log.Fatalf("failed to read file: %v", err)
+	}
+	contents := make(map[string]interface{})
+	_ = json.Unmarshal(data, &contents)
+	return contents, nil
+}
 
-// 	// Authentication for local development
-// 	cred, err := azidentity.NewDefaultAzureCredential(nil)
-// 	if err != nil {
-// 		log.Fatalf("failed to obtain a credential: %v", err)
-// 	}
+// createResourceGroup creates an http pipeline and checks if the resource group already exists.
+// If the resource group exists no error is return. If the resource group does not exists, one is created.
+func createResourceGroup(
+	ctx context.Context,
+	cred azcore.TokenCredential,
+	subscriptionId string,
+	resourceGroupName string,
+	resourceGroupLocation string,
+) error {
+	// first time do not have any logging
+	// add the logs to this first function then go back and add them to the second function
+	log.Print("creatingResourceGroup called...")
 
-// 	exits, err := checkExistenceResourceGroup(ctx, cred, resourceGroupName)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	log.Println("resources group already exist:", exits)
+	resourceGroupClient, err := armresources.NewResourceGroupsClient(subscriptionId, cred, nil)
+	if err != nil {
+		return fmt.Errorf("failed to create http pipeline client instance: %v", err)
+	}
+	log.Print("resource group http client created successfully!")
 
-// 	c.JSON(http.StatusOK, gin.H{
-// 		"message": exits,
-// 	})
-// }
+	// Add this later
+	// boolResp, err := resourceGroupClient.CheckExistence(ctx, resourceGroupName, nil)
+	// if err != nil {
+	// 	return fmt.Errorf("error while checking if resource group already exists: %v", err)
+	// } else if boolResp.Success {
+	// 	log.Printf("requested resource group already exists: %v", resourceGroupName)
+	// 	return nil
+	// } else {
+	// 	log.Printf("requested resource group does not yet exist and will been created: %v", resourceGroupName)
+	// }
 
-// func checkExistenceResourceGroup(ctx context.Context, cred azcore.TokenCredential, resourceGroupName string) (bool, error) {
-// 	resourceGroupClient, err := armresources.NewResourceGroupsClient(subscriptionID, cred, nil)
-// 	if err != nil {
-// 		return false, err
-// 	}
+	resourceGroupResponse, err := resourceGroupClient.CreateOrUpdate(ctx, resourceGroupName, armresources.ResourceGroup{Location: to.Ptr(resourceGroupLocation)}, nil)
+	if err != nil {
+		return fmt.Errorf("error while creating a new resource group: %v", err)
+	}
+	log.Printf("resource group has been created: %v", *resourceGroupResponse.ResourceGroup.Name)
 
-// 	boolResp, err := resourceGroupClient.CheckExistence(ctx, resourceGroupName, nil)
-// 	if err != nil {
-// 		return false, err
-// 	}
+	return nil
+}
 
-// 	return boolResp.Success, nil
-// }
+// ....
+func createVirtualNetwork(
+	ctx context.Context,
+	cred azcore.TokenCredential,
+	subscriptionId string,
+	resourceGroupName string,
+	resourceGroupLocation string,
+) error {
+	log.Print("createVirtualNetwork called...")
+
+	virtualNetworkClient, err := armnetwork.NewVirtualNetworksClient(subscriptionId, cred, nil)
+	if err != nil {
+		return fmt.Errorf("failed to create http pipeline client instance: %v", err)
+	}
+	log.Print("virtual network http client instance created successfully!")
+
+	virtualNetworkName := resourceGroupName + "-ASE-VNET" //practice contactnation
+
+	virtualNetwork := armnetwork.VirtualNetwork{ //learn about building structs
+		Location: to.Ptr(resourceGroupLocation),
+		Properties: &armnetwork.VirtualNetworkPropertiesFormat{
+			AddressSpace: &armnetwork.AddressSpace{
+				AddressPrefixes: []*string{
+					to.Ptr("10.1.0.0/16"),
+				},
+			},
+		},
+	}
+
+	pollerResp, err := virtualNetworkClient.BeginCreateOrUpdate(ctx, resourceGroupName, virtualNetworkName, virtualNetwork, nil)
+	if err != nil {
+		return fmt.Errorf("error while creating the poller: %v", err)
+	}
+
+	resp, err := pollerResp.PollUntilDone(ctx, nil)
+	if err != nil {
+		return fmt.Errorf("error while creating or updating the virtual network: %v", err)
+	}
+	log.Printf("virtual network has been created: %v", *resp.VirtualNetwork.Name)
+
+	return nil
+}
