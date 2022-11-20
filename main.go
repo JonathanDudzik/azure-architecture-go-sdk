2014d27bf777b63b6d03ee43537fea0a1e48c950
JonathanDudzik <jonathan.d.wesley@gmail.com>
2022-11-20 08:56:07 -0500

    Basic template working

--- main.go
+++ main.go
@@ -4,14 +4,13 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"io/ioutil" //this is deplicated?
 	"log"
 	"os"
 
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
 	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
-	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/network/armnetwork"
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/resources/armresources"
 )
 
@@ -28,69 +27,34 @@ func main() {
 	subscriptionID := os.Getenv("AZURE_SUBSCRIPTION_ID")
 	resourceGroupName := os.Getenv("AZURE_RESOURCE_GROUP")
 	resourceGroupLocation := os.Getenv("AZURE_RESOURCE_LOCATION")
+	deploymentName := "deployARM-how-to"
 	ctx := context.Background()
 
 	// at first do not return anything
 	// then return just an error (no new vars)
 	// then return an error and a value
 	err = createResourceGroup(ctx, cred, subscriptionID, resourceGroupName, resourceGroupLocation)
-
 	// simple err first, then formatted
 	if err != nil {
 		log.Fatalf("Failed at createResourceGroup: %v", err)
 	} //show with a then statement but then explain the convention of just a new line before the if err message
 
-	err = createVirtualNetwork(ctx, cred, subscriptionID, resourceGroupName, resourceGroupLocation)
-	if err != nil {
-		log.Fatalf("Failed createVirtualNetwork: %v", err)
-	}
-
-	templateFile := "template.json"
-	deploymentName := "deployARM-how-to"
-
-	template, err := readJSON(templateFile)
-	if err != nil {
-		return
-	}
-
-	deploymentsClient, err := armresources.NewDeploymentsClient(subscriptionID, cred, nil)
-	if err != nil {
-		log.Fatalf("failed to create client: %v", err)
-	}
-
-	poller, err := deploymentsClient.BeginCreateOrUpdate(
-		ctx,
-		resourceGroupName,
-		deploymentName,
-		armresources.Deployment{
-			Properties: &armresources.DeploymentProperties{
-				Mode:       to.Ptr(armresources.DeploymentModeIncremental),
-				Parameters: map[string]interface{}{},
-				Template:   template,
-			},
-		},
-		nil,
-	)
-	if err != nil {
-		log.Fatalf("failed to deploy template: %v", err)
-	}
-
-	res, err := poller.PollUntilDone(ctx, nil)
-	if err != nil {
-		log.Fatalf("failed to pull the result: %v", err)
-	}
-
-	fmt.Println(res)
+	_ = deployTemplate(ctx, cred, subscriptionID, resourceGroupName, deploymentName)
 }
 
 func readJSON(path string) (map[string]interface{}, error) {
-	data, err := ioutil.ReadFile(path)
+	templateFile, err := ioutil.ReadFile(path)
 	if err != nil {
-		log.Fatalf("failed to read file: %v", err)
+		return nil, err
 	}
-	contents := make(map[string]interface{})
-	_ = json.Unmarshal(data, &contents)
-	return contents, nil
+
+	template := make(map[string]interface{})
+	// explain how you can define the var in this if statement
+	if err := json.Unmarshal(templateFile, &template); err != nil {
+		return nil, err
+	}
+
+	return template, nil
 }
 
 // createResourceGroup creates an http pipeline and checks if the resource group already exists.
@@ -113,15 +77,15 @@ func createResourceGroup(
 	log.Print("resource group http client created successfully!")
 
 	// Add this later
-	// boolResp, err := resourceGroupClient.CheckExistence(ctx, resourceGroupName, nil)
-	// if err != nil {
-	// 	return fmt.Errorf("error while checking if resource group already exists: %v", err)
-	// } else if boolResp.Success {
-	// 	log.Printf("requested resource group already exists: %v", resourceGroupName)
-	// 	return nil
-	// } else {
-	// 	log.Printf("requested resource group does not yet exist and will been created: %v", resourceGroupName)
-	// }
+	boolResp, err := resourceGroupClient.CheckExistence(ctx, resourceGroupName, nil)
+	if err != nil {
+		return fmt.Errorf("error while checking if resource group already exists: %v", err)
+	} else if boolResp.Success {
+		log.Printf("requested resource group already exists: %v", resourceGroupName)
+		return nil
+	} else {
+		log.Printf("requested resource group does not yet exist and will been created: %v", resourceGroupName)
+	}
 
 	resourceGroupResponse, err := resourceGroupClient.CreateOrUpdate(ctx, resourceGroupName, armresources.ResourceGroup{Location: to.Ptr(resourceGroupLocation)}, nil)
 	if err != nil {
@@ -132,45 +96,65 @@ func createResourceGroup(
 	return nil
 }
 
-// ....
-func createVirtualNetwork(
+func deployTemplate(
 	ctx context.Context,
 	cred azcore.TokenCredential,
 	subscriptionId string,
 	resourceGroupName string,
-	resourceGroupLocation string,
+	deploymentName string,
 ) error {
-	log.Print("createVirtualNetwork called...")
+	// show how to temporarily ignore a value
+	template, _ := readJSON("template.json")
 
-	virtualNetworkClient, err := armnetwork.NewVirtualNetworksClient(subscriptionId, cred, nil)
+	deploymentsClient, err := armresources.NewDeploymentsClient(subscriptionId, cred, nil)
 	if err != nil {
-		return fmt.Errorf("failed to create http pipeline client instance: %v", err)
+		log.Fatalf("failed to create client: %v", err)
 	}
-	log.Print("virtual network http client instance created successfully!")
 
-	virtualNetworkName := resourceGroupName + "-ASE-VNET" //practice contactnation
+	// boolResp, err := deploymentsClient.CheckExistence(ctx, resourceGroupName, deploymentName, nil)
 
-	virtualNetwork := armnetwork.VirtualNetwork{ //learn about building structs
-		Location: to.Ptr(resourceGroupLocation),
-		Properties: &armnetwork.VirtualNetworkPropertiesFormat{
-			AddressSpace: &armnetwork.AddressSpace{
-				AddressPrefixes: []*string{
-					to.Ptr("10.1.0.0/16"),
-				},
+	poller, err := deploymentsClient.BeginCreateOrUpdate(
+		ctx,
+		resourceGroupName,
+		deploymentName,
+		armresources.Deployment{
+			Properties: &armresources.DeploymentProperties{
+				Mode:     to.Ptr(armresources.DeploymentModeIncremental),
+				Template: template,
 			},
 		},
-	}
-
-	pollerResp, err := virtualNetworkClient.BeginCreateOrUpdate(ctx, resourceGroupName, virtualNetworkName, virtualNetwork, nil)
+		nil,
+	)
 	if err != nil {
-		return fmt.Errorf("error while creating the poller: %v", err)
+		log.Fatalf("failed to deploy template: %v", err)
 	}
 
-	resp, err := pollerResp.PollUntilDone(ctx, nil)
+	res, err := poller.PollUntilDone(ctx, nil)
 	if err != nil {
-		return fmt.Errorf("error while creating or updating the virtual network: %v", err)
+		log.Fatalf("failed to pull the result: %v", err)
 	}
-	log.Printf("virtual network has been created: %v", *resp.VirtualNetwork.Name)
 
+	fmt.Println(res)
+
+	// pollerResp, err := deploymentsClient.BeginValidate(
+	// 	ctx,
+	// 	resourceGroupName,
+	// 	deploymentName,
+	// 	armresources.Deployment{
+	// 		Properties: &armresources.DeploymentProperties{
+	// 			Template:   template,
+	// 			Parameters: params,
+	// 			Mode:       to.Ptr(armresources.DeploymentModeIncremental),
+	// 		},
+	// 	},
+	// 	nil)
+	// if err != nil {
+	// 	return nil, err
+	// }
+	// resp, err := pollerResp.PollUntilDone(ctx, nil)
+	// if err != nil {
+	// 	return nil, err
+	// }
+	// return &resp.DeploymentValidateResult, nil
 	return nil
 }
